5265aebabfda08d458d9795f7b9707db994251d1
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2024-02-06 14:58:06 +0530

    Database Connection Established

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express()
 
-app.get('/ping',(req,res)=>{
-    res.send({'message' : 'PONG'})
+const startDatabase = async () => {
+    mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0.ihuv6fv.mongodb.net/Spoilers?retryWrites=true&w=majority')
+}
+
+startDatabase()
+
+const isConnected = () => {
+    return mongoose.connection.readyState === 1;
+  }
+
+app.get('/',(req,res)=>{
+    res.json({
+        database: 'MongoDB',
+        status: isConnected() ? 'connected' : 'disconnected'
+      })
 })
 
 app.listen(3000, () => {
