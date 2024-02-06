c19d5ad6debd74a4fdefa020bd4d7e19bd1ba433
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2024-02-06 15:16:25 +0530

    Database Connection Established

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 const express = require('express');
+require('dotenv').config()
 const app = express()
 
 const startDatabase = async () => {
-    mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0.ihuv6fv.mongodb.net/Spoilers?retryWrites=true&w=majority')
+    mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Connected To MongoDB')
+    })
+    .catch((err)=>{
+        console.log('Connection Interuupted Due To Error',err.message)
+    })
 }
 
 startDatabase()
