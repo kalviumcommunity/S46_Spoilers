0043458b1072904d5f570cd13cc32d0ef135aed2
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2024-02-08 14:07:05 +0530

    React app set up and created a landing page for about my ASAP idea

---
+++ frontend/react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <header>
+        <h1>SPOILERS</h1>
+      </header>
+
+      <h3>Are you sure you want to spoil your own Life ?</h3>
+
+      <h3>Alright then , stay tuned and we'll update this site with so many spoilers you'll never need !</h3>
+
+    </>
+  )
+}
+
+export default App
